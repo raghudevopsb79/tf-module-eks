161e62328fe5c866946c12478b62be6485e5c77a
SVR Kalluru <raghuk.vit@gmail.com>
2024-07-02 08:27:32 +0530

    Expense Project Terraform

--- main.tf
+++ main.tf
@@ -38,4 +38,18 @@ resource "aws_eks_node_group" "node-groups" {
   tags          = local.eks_tags
 }
 
+data "external" "oidc-thumbprint" {
+  program = [
+    "/usr/bin/kubergrunt", "eks", "oidc-thumbprint", "--issuer-url", "${aws_eks_cluster.main.identity[0].oidc[0].issuer}"
+  ]
+}
 
+resource "aws_iam_openid_connect_provider" "eks" {
+  url = aws_eks_cluster.main.identity[0].oidc[0].issuer
+
+  client_id_list = [
+    "sts.amazonaws.com",
+  ]
+
+  thumbprint_list = [data.external.oidc-thumbprint.result.thumbprint]
+}
