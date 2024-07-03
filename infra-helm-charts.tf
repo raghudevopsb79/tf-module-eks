a13a4be4a051b253372fdbae97f53f1ad29ed5df
SVR Kalluru <raghuk.vit@gmail.com>
2024-07-03 08:02:50 +0530

    Expense Project Terraform

--- infra-helm-charts.tf
+++ infra-helm-charts.tf
@@ -3,7 +3,7 @@ resource "null_resource" "get-kubeconfig" {
   depends_on = [aws_eks_node_group.node-groups, aws_eks_cluster.main]
 
   provisioner "local-exec" {
-    command =<<EOF
+    command = <<EOF
 aws eks update-kubeconfig --name "${var.env}-eks"
 EOF
   }
@@ -14,11 +14,17 @@ data "http" "metric-server" {
   url = "https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml"
 }
 
+data "kubectl_file_documents" "metric-server" {
+  content = data.http.metric-server.body
+}
+
 resource "kubectl_manifest" "metric-server" {
   depends_on = [null_resource.get-kubeconfig]
-  yaml_body = data.http.metric-server.body
+
+  count     = length(data.kubectl_file_documents.metric-server.documents)
+  yaml_body = data.kubectl_file_documents.metric-server.documents[count.index]
   force_new = true
-  wait = true
+  wait      = true
 }
 
 # resource "kubectl_manifest" "cluster-autoscaler" {
